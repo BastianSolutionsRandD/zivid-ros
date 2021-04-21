37b20b49e62b1a7b9b6729c91afaae4e53efe5a6
marqrazz <marq87gt@gmail.com>
2021-04-21 08:48:15 -0600

    trigger changes and manual intrinsic calibration

--- zivid_samples/scripts/sample_capture.py
+++ zivid_samples/scripts/sample_capture.py
@@ -6,6 +6,7 @@ import dynamic_reconfigure.client
 from zivid_camera.srv import *
 from std_msgs.msg import String
 from sensor_msgs.msg import PointCloud2
+from std_srvs.srv import Trigger, TriggerResponse
 
 
 class Sample:
@@ -19,6 +20,7 @@ class Sample:
         rospy.Subscriber("/zivid_camera/points/xyzrgba", PointCloud2, self.on_points)
 
         self.capture_service = rospy.ServiceProxy("/zivid_camera/capture", Capture)
+        self.trigger_service = rospy.Service('/zivid1/trigger', Trigger, self.trigger_response)
 
         rospy.loginfo("Enabling the reflection filter")
         settings_client = dynamic_reconfigure.client.Client("/zivid_camera/settings/")
@@ -29,12 +31,22 @@ class Sample:
         acquisition_0_client = dynamic_reconfigure.client.Client(
             "/zivid_camera/settings/acquisition_0"
         )
+        acquisition_0_client_color_settings = dynamic_reconfigure.client.Client(
+            "/zivid_camera/settings"
+        )
         acquisition_0_config = {
             "enabled": True,
-            "aperture": 5.66,
-            "exposure_time": 20000,
+            "aperture": 3.67,
+            "exposure_time": 50000,
+            "gain": 1.0,
+            "brightness": 1.0,
+        }
+        acquisition_0_config_color = {
+            "processing_color_balance_blue": 1.29,
+            "processing_color_gamma": 0.8,
         }
         acquisition_0_client.update_configuration(acquisition_0_config)
+        acquisition_0_client_color_settings.update_configuration(acquisition_0_config_color)
 
     def capture(self):
         rospy.loginfo("Calling capture service")
@@ -42,7 +54,14 @@ class Sample:
 
     def on_points(self, data):
         rospy.loginfo("PointCloud received")
+#        self.capture()
+
+    def trigger_response(self,request):
         self.capture()
+        return TriggerResponse(
+            success=True,
+            message="Hey, roger that; we'll be right there!"
+        )
 
 
 if __name__ == "__main__":
